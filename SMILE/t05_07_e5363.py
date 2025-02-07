103c13193d81acafa1e0524a6f05d771c1adbab1
Polina Puchek <polinapuchek89@gmail.com>
2025-02-07 02:33:26 +0200

    done

---
+++ SMILE/t05_07_e5363.py
@@ -0,0 +1,29 @@
+def drying(n, a, k):
+    left, right = 0, max(a)
+
+    if k <= 1:
+        return right
+
+    while right - left > 1:
+        middle = (right + left) // 2
+        dry = 0
+
+        for i in range(n):
+            if a[i] > middle:
+                dry += (a[i] - middle + k - 2) // (k - 1)
+            if dry > middle:
+                break
+
+        if dry > middle:
+            left = middle
+        else:
+            right = middle
+
+    return right
+
+
+n = int(input())
+a = list(map(int, input().split()))
+k = int(input())
+
+print(drying(n, a, k))
