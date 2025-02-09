fca99b2ee2e3bc1c6f21008c8fed07c4ca6b52cd
Polina Puchek <polinapuchek89@gmail.com>
2025-02-09 21:04:01 +0200

    done

--- SMILE/t05_07_e5363.py
+++ SMILE/t05_07_e5363.py
@@ -1,15 +1,15 @@
 def drying(n, a, k):
     left, right = 0, max(a)
 
-    if k <= 1:
+    if k == 1:
         return right
 
     while right - left > 1:
         middle = (right + left) // 2
-        dry = 0
+        dry = 0     #додаткові хвилини для батареї
 
         for i in range(n):
-            if a[i] > middle:
+            if a[i] > middle:       #якщо річ має більше води, то треба батарея
                 dry += (a[i] - middle + k - 2) // (k - 1)
             if dry > middle:
                 break
